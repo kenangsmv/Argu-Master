13c35cd7f056c38bdde4cd61fc9e93dfcb829eb0
mertyjsx <orkunmertgs41@gmail.com>
2021-03-22 13:06:30 +0300

    s

--- components/flow/TrendFlow.jsx
+++ components/flow/TrendFlow.jsx
@@ -2,38 +2,33 @@ import React, { useState, useEffect } from "react";
 import styles from "./flow.module.css";
 import PostCard from "../post/postcard.js";
 import CreateDebate from "../post/createdebate";
-import Slider from "../Slider/slider"
+import Slider from "../Slider/slider";
 
-function flow({debates,isProfile}) {
-  
-
-  const sortTıme=(a,b)=>{return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()}
-
-
+function flow({ debates, isProfile }) {
+  const sortTıme = (a, b) => {
+    return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+  };
 
   return (
     <div className={styles.flowContainer}>
-      { !debates? (
+      {!debates ? (
         <div>yükleniyor</div>
       ) : (
-        <div className={`w100 center column  ${isProfile?"": "border-left border-right"}`}>
-          {isProfile?null:<Slider></Slider>}
-          <CreateDebate></CreateDebate>
+        <div
+          className={`w100 center column  
+           border-left border-right
+          `}
+        >
+          {isProfile ? null : <Slider></Slider>}
+          {isProfile ? null : <CreateDebate></CreateDebate>}
 
-          {
-
-              debates?.sort(sortTıme).map(post=>   <PostCard key={post.id}   data={post}   ></PostCard>)
-          }
-
-
-       
-       
+          {debates?.sort(sortTıme).map((post) => (
+            <PostCard key={post.id} data={post}></PostCard>
+          ))}
         </div>
       )}
     </div>
   );
 }
 
-
-
-export default flow
+export default flow;
--- components/post/postcard.js
+++ components/post/postcard.js
@@ -1,16 +1,12 @@
-import { Divider, Button } from "@material-ui/core";
+
 import Profile from "../../images/user1.jpg";
-import Like from "../../icons/like.png";
-import Comments from "../../icons/comments.png";
-import Gots from "../../icons/dots.png";
-import Voting from '../post/voting'
-import moment from "moment"
-import Link from "next/link"
-import Live from '../../icons/live.svg'
-import VS from "../deabteroomComponents/vs/versus"
 
-const Postcard = ({data}) => {
+import Voting from "../post/voting";
+import moment from "moment";
 
+import VS from "../deabteroomComponents/vs/versus";
+
+const Postcard = ({ data }) => {
   const calculateScore = () => {
     const { topic1, topic2 } = data ? data : {};
     let topic1Score = 0;
@@ -32,39 +28,37 @@ const Postcard = ({data}) => {
     return { topic1Score, topic2Score, topic1, topic2 };
   };
 
-
- 
   return (
     <div className={"postCard boxColor column p1 mb1 box-shadow"}>
       <div className={"post w100 relative"}>
         <div className="postCardLive row center">
-<div className="redCircle"></div>
-          <span> {data.user_count>0?data.user_count:0}</span>
+          <div className="redCircle"></div>
+          <span> {data.user_count > 0 ? data.user_count : 0}</span>
         </div>
         <div className={"postTop w100"}>
           <div className={"topConent boxColor row center"}>
             <img className={"image"} src={Profile} alt="" />
-            <div className="topTextContent w100 column pl05">
+            <div className="topTextContent w100 space-between column pl05">
               <h5>{data.post_holder_name} </h5>
 
-              <time >{moment(data.createdAt).fromNow()}</time>
-                
+              <time>{moment(data.createdAt).fromNow()}</time>
             </div>
           </div>
         </div>
         <div className={"postText w100"}>
-          <span className={"p1"}>
-          {data.title}
-          </span>
+          <span className={"p1"}>{data.title}</span>
         </div>
-        <Voting  data={{topic1:data.topic1,topic2:data.topic2,id:data.string_id,categories:data.categories}} ></Voting>
-       
-        <div
-          className={
-            " w100 mt1"
-          }
-        >
-         <VS   scores={calculateScore()}></VS>
+        <Voting
+          data={{
+            topic1: data.topic1,
+            topic2: data.topic2,
+            id: data.string_id,
+            categories: data.categories,
+          }}
+        ></Voting>
+
+        <div className={" w100 mt1"}>
+          <VS scores={calculateScore()}></VS>
         </div>
       </div>
     </div>
