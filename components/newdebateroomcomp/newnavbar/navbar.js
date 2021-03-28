649c47da07524b5ea4c813e7f92cf1685757b77d
kenangsmv <rio.kenan@gmail.com>
2021-03-28 19:42:00 +0300

    cek

--- components/newdebateroomcomp/newnavbar/navbar.js
+++ components/newdebateroomcomp/newnavbar/navbar.js
@@ -1,27 +1,55 @@
 import React from 'react'
 import Logo from '../../../icons/logo.svg'
-import Home from '../../../icons/home.png'
-import Trending from '../../../icons/fire.png'
-import Saved from '../../../icons/bookmark.png'
+import Home from '@material-ui/icons/Home'
+import Fire from '@material-ui/icons/Whatshot'
+import Saved from '@material-ui/icons/Bookmark'
+import Viewed from '@material-ui/icons/RemoveRedEye'
+import Profile from '@material-ui/icons/Person';
 import ProfilePhoto from '../../../images/profilephoto.jpg'
+import { IconButton } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
 export default function navbar() {
+  
     return (
         <div className="newNavbar column space-between">
-                   
-        <img className="newnavbarlogo" src={Logo} alt=""/>
-        
-        <div className="newNavbarMenu column center">
-            <div className=" center">
-                <img className="newicons" src={Home} alt=""/>
-                </div>
-                <div className=" center">
-                <img className="newicons" src={Trending} alt=""/>
-                </div>
-                <div className=" center">
-                <img className="newicons" src={Saved} alt=""/>
-                </div>
+                   <div className="newnavbarlogo center">
+        <img  src={Logo} alt=""/>
         </div>
-        <div className="newNavbarButtom ">
+        <div className="newNavbarMenu column center">
+            <div className="iconHover row space-between">
+            <IconButton className="iconBackground" >
+           <Home  />
+           </IconButton>
+           <div className="iconHoverBorder"></div>
+           </div>
+           <div className="iconHover row space-between">
+            <IconButton className="iconBackground" >
+           <Fire  />
+           </IconButton>
+           <div className="iconHoverBorder"></div>
+           </div>
+           <div className="iconHover row space-between">
+            <IconButton className="iconBackground" >
+           <Saved  />
+           </IconButton>
+           <div className="iconHoverBorder "></div>
+           </div>
+           <div className="iconHover row space-between">
+            <IconButton className="iconBackground" >
+           <Viewed  />
+           </IconButton>
+           <div className="iconHoverBorder"></div>
+           </div>
+           <div className="iconHover row space-between">
+            <IconButton className="iconBackground" >
+           <Profile  />
+           </IconButton>
+           <div className="iconHoverBorder"></div>
+           </div>
+      
+        </div>
+        <div className="newNavbarButtom center">
                  <img src={ProfilePhoto} alt=""/>
         </div>
         </div>
